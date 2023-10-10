07be6c66abf2d2ec3563f93edd48329410612c5c
Shon Rice <shonricedata@outlook.com>
2023-10-09 20:51:42 -0700

    First undertaking of proper JS coding.

---
+++ coding_practice/script.js
@@ -0,0 +1,13 @@
+const country = "United States";
+let continent = "North America";
+let population = "333 million";
+const isIsland = false;
+let language = "";
+
+console.log(typeof country)
+console.log(typeof continent)
+console.log(typeof population)
+console.log(typeof isIsland)
+console.log(typeof language)
+
+language = "English"
